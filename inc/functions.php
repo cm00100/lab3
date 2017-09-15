3173c773d5f030f1eae9c32c6b7f325814809119
Nolan <noguzman@csumb.edu>
2017-09-15 20:40:56 +0000

    displayCard function

--- inc/functions.php
+++ inc/functions.php
@@ -1,18 +1,12 @@
 <?php
 
-echo "hi";
 
+function displayCards(){
+    $suite=rand(1,4);
+    $card=rand(1,13);
+    echo "<img id='$card' src='../img/cards/$suite/$card.png' alt='$card' title='". ucfirst($card) . "' width='70' />";
+}
+displayCards();
 ?>
 
 
-
-<!DOCTYPE html>
-<html>
-    <head>
-        <title> Silverjack </title>
-    </head>
-    <body>
-        
-        
-    </body>
-</html>
